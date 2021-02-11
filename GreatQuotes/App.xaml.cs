9264d144ca79346f0521d11deaaeb691c12211ca
Jason Jamieson <jjamieson@OST-C02DN1D3MD6M.localdomain>
2021-02-11 11:25:15 -0500

    initial commit

---
+++ GreatQuotes/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using GreatQuotes.Data;
+using GreatQuotes.ViewModels;
+using GreatQuotes.Views;
+using Xamarin.Forms;
+
+namespace GreatQuotes
+{
+    public partial class App : Application
+    {
+        public static MainViewModel GreatQuotesViewModel { get; internal set; }
+
+        public App()
+        {
+            InitializeComponent();
+            GreatQuotesViewModel = new MainViewModel();
+            MainPage = new NavigationPage (new QuoteListPage());
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            QuoteManager.Instance.Save();
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
